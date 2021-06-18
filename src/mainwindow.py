e5c4aeefd6311c1924cb549ff1c60f68beb681d4
pCarranzaVelez <42421191+pCarranzaVelez@users.noreply.github.com>
2021-06-18 02:23:49 -0300

    Gui comienza a tomar forma

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -1,5 +1,5 @@
 # PyQt5 modules
-from PyQt5.QtWidgets import QMainWindow, QFileDialog, QInputDialog, QApplication, QWidget, QPushButton, QAction, \
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QDialog, QApplication, QWidget, QPushButton, QAction, \
     QLineEdit, \
     QMessageBox
 from PyQt5.QtCore import QCoreApplication, QObject, QRunnable, QThread, QThreadPool, pyqtSignal, pyqtSlot
@@ -8,6 +8,10 @@ from PyQt5.QtGui import QFont
 
 # Python modules
 import matplotlib.pyplot as plt
+import sounddevice as sd
+from scipy.io.wavfile import write
+from pydub import AudioSegment
+import os
 
 # Project modules
 from src.ui.mainwindow import Ui_MainWindow
@@ -17,8 +21,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.setFixedSize(1200, 600)
         self.setupUi(self)
         self.openbtn.clicked.connect(self.openFile)
+        self.recordbtn.clicked.connect(self.StartRecording)
         self.filename = None
 
     def openFile(self):
@@ -37,6 +43,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.filename = QFileDialog.getOpenFileName(self, "Abrir Archivo", "", "Archivo de Audio (*.mp3, *.wav, "
                                                                                    "*.ogg)",
                                                         "Archivo de Audio (*.mp3, *.wav, *.ogg)")[0]
+
+    def StartRecording(self, seconds = 3, fs = 44100):
+        open = True
+        if self.filename:
+            open = False
+            msgbox = QMessageBox(QMessageBox.Question, "Confirmación",
+                                 "¿Seguro que quiere realizar una grabación?\nSe perderá el progreso actual.")
+            msgbox.addButton(QMessageBox.Yes)
+            msgbox.addButton(QMessageBox.No)
+            msgbox.setDefaultButton(QMessageBox.No)
+            reply = msgbox.exec()
+            if reply == QMessageBox.Yes:
+                open = True
+        if open:
+            d = QDialog()
+            d.setWindowTitle("Recording...")
+            d.resize(300, 200)
+            d.show()
+            myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+            sd.wait()  # Wait until recording is finished
+            d.close()
+            current_path = os.path.dirname(os.path.abspath(__file__))
+            self.filename = os.path.join(current_path, "recording.ogg")
+            write(self.filename, fs, myrecording)  # Save as WAV file
 
     def spectrogram(self, data):
 
